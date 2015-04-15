03d2c0143efbd81b0065a00a70259b7710ccdf2d
alexlebourg <alexlebourg@localhost>
2015-04-15 23:01:10 +0200

    15 avril

---
+++ accueil.php
@@ -0,0 +1,34 @@
+<?php
+?>
+
+<html>
+
+<head>
+
+
+<meta charset="utf-8">
+
+
+
+	<link rel="stylesheet" href="style.css">
+	
+	
+<title>Management Connexion</title>
+
+
+</head>
+
+<body>
+<div id="test">
+
+va te faire enculer
+
+</div>
+
+</body>
+
+
+
+
+
+</html>
